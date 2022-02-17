41b0ffe531237df17ba5443972f3a17ef5fc5257
ShelycaSurrayensih <shelysurrayensih0501@gmail.com>
2022-02-17 13:54:31 +0700

    Pertemuan 1 - Modern JavaScript

---
+++ Pertemuan 1/template.js
@@ -0,0 +1,11 @@
+let fname = 'Cristian';
+let Iname = 'Ronaldo';
+let age = prompt("Masukkan umur Cristian Ronaldo!");
+
+// Cara lama
+// let result = frane + ' ' + Iname + 'is' + age + 'years old';
+// alert(result);
+
+// Memakai template strings
+let result = `${fname} ${Iname} is ${age} years old`;
+alert(result);
